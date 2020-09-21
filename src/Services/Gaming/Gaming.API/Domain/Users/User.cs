beb2cb63b318e78d7b8f061d55079167c3e418f1
Deliay <admin@remiliascarlet.com>
2020-09-22 02:06:02 +0800

    Add 'User' entity

---
+++ src/Services/Gaming/Gaming.API/Domain/Users/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Gaming.API.Domain.Users
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string NickName { get; set; }
+        public string Email { get; set; }
+        public DateTimeOffset RegisteredAt { get; set; }
+    }
+}
